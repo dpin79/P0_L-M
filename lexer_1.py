fc1ff9a0589327f1902aa082a337ac7b0791f8ec
Daniel Piñeros <d.pinerosm@uniandes.edu.co>
2023-08-23 17:28:55 -0500

    Update

---
+++ lexer_1.py
@@ -0,0 +1,20 @@
+"""
+
+La idea principal del LEXER es convertir las palabras que se espera
+recibir a tokens conocidos y estandarizar lo que es recibido por 
+parametros para posteriormente ser analizado sintacticamente por el PARSER.
+
+"""
+"""
+
+###ACLARACIONES LITERALES DEL 
+
+"""
+def lexer(input:str):
+    strConverted = ""
+
+
+    ###LÓGICA PARA TOKENIZAR CADENA
+
+
+    return strConverted
